f9e1e4d66660e292d8ee16d6ebcf6a81b1679247
taki.lnt <taki.lnt@b50f086b-2709-fbe3-7840-ea9a80ccd57a>
2010-06-02 17:41:32 +0000

    Xong 1 phần template, mặc dù mới xong 1 phần nhưng đã có thể sử dụng để làm các trang khác rồi. Sẽ tiếp tục hoàn thiện bằng php. Các bạn lấy templates về tạo trang giao diện và làm theo phân công :D

--- trunk/ webnhacso/Templates/TrangChu.dwt.php
+++ trunk/ webnhacso/Templates/TrangChu.dwt.php
@@ -2,13 +2,68 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!-- TemplateBeginEditable name="doctitle" -->
-<title>Untitled Document</title>
-<!-- TemplateEndEditable -->
-<!-- TemplateBeginEditable name="head" -->
-<!-- TemplateEndEditable -->
+<title>Fantasy Music</title>
+<link href="../css/giaodien.css" rel="stylesheet" type="text/css"> 
 </head>
 
 <body>
+<div class="banner" id="idBanner" align="center">
+<img src="../images/header-02.jpg" width="1000">
+</div>
+<div class="main" id="idmain">
+	
+   <table width="100%" border="0" cellspacing="0">
+  <tr>
+    <td class="left-col">
+    	 <div class="left-header" id="idLeftMenu" align="center">
+        	Menu
+      	 </div>
+          <table height="90" width="180" border="0" cellspacing="0">
+          <tr>
+            <td>Trang Chủ</td>
+          </tr>
+          <tr>
+            <td>Đăng/Upload bài hát mới</td>
+          </tr>
+          <tr>
+            <td>Đăng ký tài khoản mới</td>
+          </tr>
+        </table>
+    	<div class="left-header" id="idLeftMenu" align="center">
+  			Danh mục bài hát
+  		</div>  
+       	<div class="left-header" id="idLeftMenu" align="center">
+        	Clip
+      	</div>
+       	<div class="left-header" id="idLeftMenu" align="center">
+        	Playlist HOT
+      	</div>
+      	<div class="left-header" id="idLeftMenu" align="center">
+        	Ca sĩ
+      	</div>
+    </td>
+    <td class="mid-col" valign="top">
+        <div id="idMainContent" class="main-content" align="center">
+        	<!-- TemplateBeginEditable name="mainConten" -->mainConten<!-- TemplateEndEditable -->
+        </div>
+    </td>
+    <td class="right-col" valign="top">
+    	<table width="100%" border="0" cellspacing="0">
+          <tr>
+            <td>form đăng nhập</td>
+          </tr>
+          <tr>
+            <td>form tìm kiếm đơn giản</td>
+          </tr>
+    	</table>
+        <div class="right-content" id="idRightContent" align="center">
+   	  		<!-- TemplateBeginEditable name="RightContent" -->RightContent<!-- TemplateEndEditable -->
+        </div>
+    </td>
+  </tr>
+</table>
+  
+</div>
+
 </body>
 </html>
